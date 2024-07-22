72047bbb1a70512188263e38e774e9a1d0b74437
trucdtd <duongdieutruc09@gmail.com>
2024-07-22 21:59:12 +0700

    Update NhaTuyenDungController.java

--- src/main/java/demo/Controllers/NhaTuyenDungController.java
+++ src/main/java/demo/Controllers/NhaTuyenDungController.java
@@ -1,16 +1,25 @@
 package demo.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import demo.services.SessionService;
+
 @Controller
 @RequestMapping("/job4u")
 public class NhaTuyenDungController {
+	@Autowired
+	SessionService sessionService;
+	
+	
 	
 	@RequestMapping("/employers")
 	public String nhaTuyenDung(){
 		
 		return"nhaTuyenDung";
 	}
+	
+	
 }
