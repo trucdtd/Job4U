1a0cac6557f7ab3db6bb489d2fdbe6c929247ad4
Lanh-04 <buivanlanh567@gmail.com>
2024-08-12 22:55:51 +0700

    code
    code

--- src/main/java/demo/services/UserService.java
+++ src/main/java/demo/services/UserService.java
@@ -36,17 +36,15 @@ import jakarta.annotation.PostConstruct;
 
 @Service
 public class UserService {
-	@Value("${spring.mail.host}")
-    private String mailHost;
-
-    @Value("${spring.mail.port}")
-    private int mailPort;
-
-    @Value("${spring.mail.username}")
-    private String mailUsername;
-
-    @Value("${spring.mail.password}")
-    private String mailPassword;
+	/*
+	 * @Value("${spring.mail.host}") private String mailHost;
+	 * 
+	 * @Value("${spring.mail.port}") private int mailPort;
+	 * 
+	 * @Value("${spring.mail.username}") private String mailUsername;
+	 * 
+	 * @Value("${spring.mail.password}") private String mailPassword;
+	 */
 	
     @Autowired
     private UserRepository userRepository;
@@ -124,73 +122,54 @@ public class UserService {
         return usersDao.existsByPhonenumber(numberphone);
     }
 
-    public boolean isTokenValid(String token, String code) {
-        UsersEntity user = userRepository.findByToken(token);
-        if (user != null) {
-            System.out.println("Token from DB: " + user.getToken());
-            System.out.println("Code entered by user: " + code);
-            return code.equals(user.getToken());
-        }
-        return false;
-    }
+	/*
+	 * public boolean isTokenValid(String token, String code) { UsersEntity user =
+	 * userRepository.findByToken(token); if (user != null) {
+	 * System.out.println("Token from DB: " + user.getToken());
+	 * System.out.println("Code entered by user: " + code); return
+	 * code.equals(user.getToken()); } return false; }
+	 * 
+	 * public void updatePassword(String token, String newPassword) { UsersEntity
+	 * user = userRepository.findByToken(token); if (user != null) {
+	 * user.setPassword(newPassword); // Nên mã hóa mật khẩu trước khi lưu
+	 * user.setToken(null); // Xóa token sau khi dùng userRepository.save(user); } }
+	 * 
+	 * private static final String CHARACTERS =
+	 * "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; private static final int TOKEN_LENGTH
+	 * = 6; private static final SecureRandom RANDOM = new SecureRandom();
+	 * 
+	 * private String generateToken() { StringBuilder token = new
+	 * StringBuilder(TOKEN_LENGTH); for (int i = 0; i < TOKEN_LENGTH; i++) { int
+	 * index = RANDOM.nextInt(CHARACTERS.length());
+	 * token.append(CHARACTERS.charAt(index)); } return token.toString(); }
+	 */
 
-    public void updatePassword(String token, String newPassword) {
-        UsersEntity user = userRepository.findByToken(token);
-        if (user != null) {
-            user.setPassword(newPassword); // Nên mã hóa mật khẩu trước khi lưu
-            user.setToken(null); // Xóa token sau khi dùng
-            userRepository.save(user);
-        }
-    }
+	/*
+	 * public String createPasswordResetToken(String email) { String token =
+	 * generateToken(); UsersEntity user = userRepository.findByEmail(email); if
+	 * (user != null) { user.setToken(token); userRepository.save(user);
+	 * 
+	 * sendPasswordResetEmail(email, token); } return token; }
+	 */
 
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static final int TOKEN_LENGTH = 6;
-    private static final SecureRandom RANDOM = new SecureRandom();
-
-    private String generateToken() {
-        StringBuilder token = new StringBuilder(TOKEN_LENGTH);
-        for (int i = 0; i < TOKEN_LENGTH; i++) {
-            int index = RANDOM.nextInt(CHARACTERS.length());
-            token.append(CHARACTERS.charAt(index));
-        }
-        return token.toString();
-    }
-
-    public String createPasswordResetToken(String email) {
-        String token = generateToken(); 
-        UsersEntity user = userRepository.findByEmail(email);
-        if (user != null) {
-            user.setToken(token);
-            userRepository.save(user);
-
-            sendPasswordResetEmail(email, token);
-        }
-        return token;
-    }
-
-    @PostConstruct
-    private void initializeMailSender() {
-        JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
-        mailSenderImpl.setHost(mailHost);
-        mailSenderImpl.setPort(mailPort);
-        mailSenderImpl.setUsername(mailUsername);
-        mailSenderImpl.setPassword(mailPassword);
-
-        Properties props = mailSenderImpl.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        this.mailSender = mailSenderImpl;
-    }
-
-    public void sendPasswordResetEmail(String userEmail, String token) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(userEmail);
-        message.setSubject("Yêu cầu đặt lại mật khẩu");
-        message.setText("Mã token của bạn để đặt lại mật khẩu là: " + token);
-
-        mailSender.send(message);
-    }
+	/*
+	 * @PostConstruct private void initializeMailSender() { JavaMailSenderImpl
+	 * mailSenderImpl = new JavaMailSenderImpl(); mailSenderImpl.setHost(mailHost);
+	 * mailSenderImpl.setPort(mailPort); mailSenderImpl.setUsername(mailUsername);
+	 * mailSenderImpl.setPassword(mailPassword);
+	 * 
+	 * Properties props = mailSenderImpl.getJavaMailProperties();
+	 * props.put("mail.transport.protocol", "smtp"); props.put("mail.smtp.auth",
+	 * "true"); props.put("mail.smtp.starttls.enable", "true");
+	 * props.put("mail.debug", "true");
+	 * 
+	 * this.mailSender = mailSenderImpl; }
+	 * 
+	 * public void sendPasswordResetEmail(String userEmail, String token) {
+	 * SimpleMailMessage message = new SimpleMailMessage();
+	 * message.setTo(userEmail); message.setSubject("Yêu cầu đặt lại mật khẩu");
+	 * message.setText("Mã token của bạn để đặt lại mật khẩu là: " + token);
+	 * 
+	 * mailSender.send(message); }
+	 */
 }
