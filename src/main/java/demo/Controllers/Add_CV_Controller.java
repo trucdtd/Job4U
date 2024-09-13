154528cb6c0f839e8bce3112aa7c870154b9aca9
LeVanDoanIT <levandoan2305@gmail.com>
2024-09-13 08:53:30 +0700

    code add cv

---
+++ src/main/java/demo/Controllers/Add_CV_Controller.java
@@ -0,0 +1,18 @@
+package demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/job4u/profile")
+public class Add_CV_Controller {
+@GetMapping("/addCV")
+public String main() {
+	
+   return "trangCaNhanNguoiTimViec";
+}
+
+}
