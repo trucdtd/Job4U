e2486a09b6cd729ae29c1436944c4e7299558426
Lanh-04 <buivanlanh567@gmail.com>
2024-11-12 16:43:58 +0700

    thanh toán
    sửa dính jobid và lấy dữ liệu từ service thay vì gáng cứng

--- src/main/java/demo/dao/ServicesDao.java
+++ src/main/java/demo/dao/ServicesDao.java
@@ -1,9 +1,13 @@
 package demo.dao;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import demo.entity.ServicesEntity;
 
 public interface ServicesDao extends JpaRepository<ServicesEntity, Integer> {
-	ServicesEntity findByServiceid(Integer serviceid);
+
 }
