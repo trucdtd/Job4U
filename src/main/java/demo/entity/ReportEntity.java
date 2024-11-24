b5e95962d39390d662aa4f4af033514f48cbef56
Lanh-04 <buivanlanh567@gmail.com>
2024-11-24 22:41:02 +0700

    bản entity repost

---
+++ src/main/java/demo/entity/ReportEntity.java
@@ -0,0 +1,46 @@
+package demo.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Report")
+public class ReportEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "reportid")
+	private Integer reportid;
+	
+	@ManyToOne
+	@JoinColumn(name = "userid", nullable = false)
+	private UsersEntity user;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "jobid", nullable = false)
+	private JoblistingsEntity job;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "employerid", nullable = false)
+	private JobSeekersEntity employer;
+	
+	@Column(name = "reason")
+    private String reason; // Lý do báo cáo
+	
+	@Column(name = "reportedat", nullable = false)
+	private LocalDate reportedat;
+
+
+}
